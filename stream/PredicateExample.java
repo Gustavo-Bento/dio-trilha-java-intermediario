70aa55f99aaf1d24f7eb7c226e809c880afd39a7
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-24 10:46:47 -0300

    stream predicate

--- stream/PredicateExample.java
+++ stream/PredicateExample.java
@@ -2,14 +2,11 @@ package stream;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class PredicateExample {
     public static void main(String[] args) {
         List<String> palavras = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
-        
-        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
 
-        palavras.stream().filter(maisDeCincoCaracteres).forEach(System.out::println);
+        palavras.stream().filter(p -> p.length() > 5).forEach(System.out::println);
     }
 }
