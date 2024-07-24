f6bf71a375e48b7326f409611382e3d67372aa28
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-24 10:48:56 -0300

    predicate com consumer

--- stream/ConsumerExample.java
+++ stream/ConsumerExample.java
@@ -7,10 +7,6 @@ public class ConsumerExample {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
-        numeros.stream().forEach(n -> {
-            if (n % 2 == 0) {
-                System.out.println(n);
-            }
-        });
+        numeros.stream().filter(n->n % 2 == 0).forEach(System.out::println);
     }
 }
