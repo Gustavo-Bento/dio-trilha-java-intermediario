99f01e60c77670b65b4e83577972ae30391abe25
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-25 10:39:42 -0300

    stream binary operator pt.2

--- stream/BinaryOperatorExample.java
+++ stream/BinaryOperatorExample.java
@@ -7,8 +7,13 @@ import java.util.function.BinaryOperator;
 public class BinaryOperatorExample {
     public static void main(String[] args) {
         List<Integer>numeros = Arrays.asList(1,2,3,4,5);
-        BinaryOperator<Integer> somar = (num1,num2)->num1+num2;
-        int resultado = numeros.stream().reduce(0, somar);
+
+        int resultado = numeros.stream().reduce(0, new BinaryOperator<Integer> (){
+            public Integer apply(Integer n1, Integer n2){
+                return n1+n2;
+            }
+        });
+
         System.out.println("A soma dos dois numeros é: "+resultado);
     }
 }
