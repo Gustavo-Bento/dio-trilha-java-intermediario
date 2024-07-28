345ed6e87e465232359fae219c2ce1c812f4f23e
Gustavo Ferreira <gfmb94@gmail.com>
2024-07-27 21:03:17 -0300

    desafio 2

--- desafio/transacoesStream/RegistroTransacoesComStream.java
+++ desafio/transacoesStream/RegistroTransacoesComStream.java
@@ -1,4 +1,4 @@
-package desafio;
+package desafio.transacoesStream;
 
 import java.util.ArrayList;
 import java.util.List;
--- desafio/transacoesStream/Transacao.java
+++ desafio/transacoesStream/Transacao.java
@@ -1,4 +1,4 @@
-package desafio;
+package desafio.transacoesStream;
 
 class Transacao {
     private char tipo;
